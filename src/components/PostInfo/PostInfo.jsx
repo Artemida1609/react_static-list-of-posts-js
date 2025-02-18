899449ac5949261a1bc745e88ee41465e4db6a7a
Artem <skickoartem74@gmail.com>
2025-02-18 16:13:58 +0100

    test

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,29 @@
-export const PostInfo = () => <>Put the post here</>;
+import { UserInfo } from '../UserInfo';
+// import { CommentList } from '../CommentList';
+// import commentsFromServer from '../../api/comments.json';
+
+// function getCommentsById(commentId) {
+//   return commentsFromServer.filter(comment => comment.postId === commentId);
+// }
+
+export const PostInfo = ({ post }) => {
+  // const postComments = getCommentsById(post.id) || [];
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+        <p>
+          {' Posted by  '}
+          {post.user && <UserInfo user={post.user} />}
+        </p>
+      </div>
+      <p className="PostInfo__body">{post.body}</p>
+      <hr />
+      {/* {postComments.length > 0 ? (
+        (<CommentList comments={postComments}/>) :
+        (<b data-cy="NoCommentsMessage">No comments yet</b>)
+      )} */}
+    </div>
+  );
+};
