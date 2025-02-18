ecd50d4c06148530c6502feea14d2a15962c8304
Artem <skickoartem74@gmail.com>
2025-02-18 16:54:07 +0100

    all done :)

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,32 +1,22 @@
-import { UserInfo } from "../UserInfo";
-import { CommentList } from "../CommentList";
-import commentsFromServer from '../../api/comments.json';
-
-function getCommentsById(commentId) {
-  return commentsFromServer.filter(comment => comment.postId === commentId);
-}
-
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
 
 export const PostInfo = ({ post }) => {
-  const postComments = getCommentsById(post.id) || [];
-
   return (
     <div className="PostInfo">
       <div className="PostInfo__header">
         <h3 className="PostInfo__title">{post.title}</h3>
         <p>
           {' Posted by  '}
-          {post.user && <UserInfo user={post.user}/>}
+          {post.user && <UserInfo user={post.user} />}
         </p>
       </div>
-      <p className="PostInfo__body">
-        {post.body}
-      </p>
+      <p className="PostInfo__body">{post.body}</p>
       <hr />
-      {postComments.length > 0 ?
-      (<CommentList comments={postComments}/>) :
-      (<b data-cy="NoCommentsMessage">No comments yet</b>)
+      {post.comments.length > 0 ? 
+        (<CommentList comments={post.comments}/>) :
+        (<b data-cy="NoCommentsMessage">No comments yet</b>)
       }
     </div>
-  )
+  );
 };
