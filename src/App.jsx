be1f74cdbefb7d0caa6b5a7e868b90f9845e6254
Artem <skickoartem74@gmail.com>
2025-02-18 17:08:37 +0100

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { PostList } from './components/PostList';
 import postsFromServer from './api/posts.json';
 import usersFromServer from './api/users.json';
 
-
 function getUserById(userId) {
   return usersFromServer.find(user => user.id === userId) || null;
 }
@@ -12,13 +11,11 @@ function getUserById(userId) {
 export const posts = postsFromServer.map(post => ({
   ...post,
   user: getUserById(post.userId),
-}))
-
-
+})).filter(post => post.user !== null);
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
-    <PostList posts={posts}/>
+    <PostList posts={posts} />
   </section>
 );
