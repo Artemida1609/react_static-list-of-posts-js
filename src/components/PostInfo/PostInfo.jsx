cfaddb94d9f7a0ee51d701cddb8de49fbe7a400d
Artem <skickoartem74@gmail.com>
2025-02-18 16:37:03 +0100

    test1

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,13 +1,14 @@
-import { UserInfo } from '../UserInfo';
-// import { CommentList } from '../CommentList';
-// import commentsFromServer from '../../api/comments.json';
+import { UserInfo } from "../UserInfo";
+import { CommentList } from "../CommentList";
+import commentsFromServer from '../../api/comments.json';
 
-// function getCommentsById(commentId) {
-//   return commentsFromServer.filter(comment => comment.postId === commentId);
-// }
+function getCommentsById(commentId) {
+  return commentsFromServer.filter(comment => comment.postId === commentId);
+}
+
 
 export const PostInfo = ({ post }) => {
-  // const postComments = getCommentsById(post.id) || [];
+  const postComments = getCommentsById(post.id) || [];
 
   return (
     <div className="PostInfo">
@@ -15,15 +16,17 @@ export const PostInfo = ({ post }) => {
         <h3 className="PostInfo__title">{post.title}</h3>
         <p>
           {' Posted by  '}
-          {post.user && <UserInfo user={post.user} />}
+          {post.user && <UserInfo user={post.user}/>}
         </p>
       </div>
-      <p className="PostInfo__body">{post.body}</p>
+      <p className="PostInfo__body">
+        {post.body}
+      </p>
       <hr />
-      {/* {postComments.length > 0 ? (
-        (<CommentList comments={postComments}/>) :
-        (<b data-cy="NoCommentsMessage">No comments yet</b>)
-      )} */}
+      {postComments.length > 0 ?
+      (<CommentList comments={postComments}/>) :
+      (<b data-cy="NoCommentsMessage">No comments yet</b>)
+      }
     </div>
-  );
+  )
 };
